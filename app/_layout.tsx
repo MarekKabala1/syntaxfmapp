c4e3eb48f292c3c298cdb68d50d7a039b5739564
MarekKabala1 <marekkabala1@gmail.com>
2025-11-12 13:51:52 +0000

    feat: implement stack navigation and home screen layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,29 @@
-import { Stack } from "expo-router";
+import { Stack } from 'expo-router';
+import { Image, ImageBackground } from 'react-native';
+
+const logoImage = <Image style={{ width: 250, height: 60 }} source={require('../assets/images/SyntaxLogoWide.png')} resizeMode='cover' />;
+const imageBg = require('../assets/images/whitegrit.png');
+
+function StackLayout() {
+	return (
+		<Stack
+			screenOptions={{
+				headerShown: true,
+				headerTitleAlign: 'center',
+				headerTitleStyle: { color: '#000' },
+			}}>
+			<Stack.Screen
+				name='index'
+				options={{
+					headerTitle: () => logoImage,
+					headerShown: false,
+					// headerBackground: () => <ImageBackground style={{ backgroundColor: '#000', minWidth: 600, height: 100 }} source={imageBg} resizeMode='cover' />,
+				}}
+			/>
+		</Stack>
+	);
+}
 
 export default function RootLayout() {
-  return <Stack />;
+	return <StackLayout />;
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,34 @@
-import { Text, View } from "react-native";
+import { router } from 'expo-router';
+import { useEffect } from 'react';
+import { ImageBackground, Text, Image, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
-export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
+const imageBg = require('../assets/images/whitegrit.png');
+
+const logoImage = <Image style={{ width: 250, height: 60 }} source={require('../assets/images/SyntaxLogoWide.png')} resizeMode='cover' />;
+
+function navigateToHomePage() {
+	setTimeout(() => {
+		router.replace('/(tabs)/home');
+	}, 3000);
+}
+
+export default function Home() {
+	useEffect(() => {
+		navigateToHomePage();
+	}, []);
+	return (
+		<SafeAreaView
+			style={{
+				flex: 1,
+				justifyContent: 'center',
+				alignItems: 'center',
+				backgroundColor: '#000',
+			}}>
+			<ImageBackground style={{ flex: 1, width: '100%', height: '100%', justifyContent: 'center', alignItems: 'center' }} source={imageBg} resizeMode='cover'>
+				<Text style={{ color: '#FABF47', fontSize: 20, fontWeight: 'bold' }}>Welcome TO </Text>
+				<View>{logoImage}</View>
+			</ImageBackground>
+		</SafeAreaView>
+	);
 }
