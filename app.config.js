8b63e0962af419ba041f36a678b1bea632d147e8
MarekKabala1 <marekkabala1@gmail.com>
2025-12-05 05:42:42 +0000

    chore: remove app.config.js and add SENTRY_AUTH_TOKEN to eas.json for environment configuration

--- app.config.js
+++
@@ -1,5 +0,0 @@
-export default ({ config }) => ({
-	...config,
-});
-export const IS_DEV = process.env.APP_VARIANT === 'development';
-export const IS_PROD = process.env.APP_VARIANT === 'production';
