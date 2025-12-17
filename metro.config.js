eee871b191091c2771dfb54981c73031fe391c3f
MarekKabala1 <marekkabala1@gmail.com>
2025-12-17 06:16:41 +0000

    chore: update metro.config.js to  remove expo-gl and expo-three from package.json and package-lock.json, and add react-native-webview and react-native-youtube-iframe dependencies

--- metro.config.js
+++ metro.config.js
@@ -1,10 +1,8 @@
-const {
-  getSentryExpoConfig
-} = require("@sentry/react-native/metro");
+const { getSentryExpoConfig } = require('@sentry/react-native/metro');
 
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getSentryExpoConfig(__dirname);
 
-config.resolver.assetExts.push('xml');
+config.resolver.assetExts.push('xml', 'glb', 'gltf');
 
 module.exports = config;
