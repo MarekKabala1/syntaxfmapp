0666dc17943ba11207202a958f3650fee193a1cb
MarekKabala1 <marekkabala1@gmail.com>
2026-01-06 06:36:32 +0000

    refactor: simplify BgWrapper component by removing platform-specific rendering and adjusting padding styles

--- components/BgWrapper.tsx
+++ components/BgWrapper.tsx
@@ -1,12 +1,12 @@
-import { ImageBackground, Platform, StyleSheet, View } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { ImageBackground, StyleSheet, View } from 'react-native';
 
 const imageBg = require('../assets/images/whitegrit.png');
 
 export default function BgWrapper({ children }: { children: React.ReactNode }) {
 	return (
 		<ImageBackground source={imageBg} style={styles.image} resizeMode='cover'>
-			{Platform.OS === 'ios' ? <View style={styles.bg}>{[children]}</View> : <SafeAreaView style={styles.bg}>{[children]}</SafeAreaView>}
+			{/* {Platform.OS === 'ios' ? <View style={styles.bg}>{[children]}</View> : <SafeAreaView style={styles.bg}>{[children]}</SafeAreaView>} */}
+			<View style={styles.bg}>{[children]}</View>
 		</ImageBackground>
 	);
 }
@@ -14,12 +14,12 @@ const styles = StyleSheet.create({
 	image: {
 		flex: 1,
 		backgroundColor: '#000',
-		paddingInline: 20,
+		// paddingInline: 20,
 	},
 	bg: {
 		flex: 1,
 		width: '100%',
-		paddingInline: 16,
+		paddingInline: 10,
 		alignItems: 'center',
 	},
 });
