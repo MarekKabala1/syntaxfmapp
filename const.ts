2b020223cea3d5d91aa0b6b39007763d9dda3840
Toxic-87 <83923087+MarekKabala1@users.noreply.github.com>
2026-01-29 06:28:19 +0000

    Add GITHUB_RAW_URL constant to const.ts

--- const.ts
+++ const.ts
@@ -1,1 +1,2 @@
 export const SYNTAX_FM_CHANNEL_ID = 'UCyU5wkjgQYGRB0hIHMwm2Sg';
+export const GITHUB_RAW_URL = 'https://raw.githubusercontent.com/MarekKabala1/syntax-snackpack-data/main/data/snackpack-list.json';
